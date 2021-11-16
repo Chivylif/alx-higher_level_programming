e0005ea92d28d065328105e1d55bb7374b4ff0d0
Osigwe Chidozie <chivy4gud@gmail.com>
2021-11-16 16:54:15 +0100

    0x01-python-if_else_loops_functions projects

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# a program that prints the ASCII alphabet, in lowercase, not followed by a new line except 'e' and 'q'
 for i in range(97, 123):
 	if i == 101 or i == 113:
 		continue
