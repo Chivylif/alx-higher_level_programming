8ac9ef5530e15ba5dfc6c4f97862286aac35ad21
Osigwe Chidozie <chivy4gud@gmail.com>
2021-11-17 16:33:14 +0100

    0x01-python-if_else_loops_functions projects

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for x in range(122, 96, -1):
-    if x % 2 is 1:
-        x = x - 32
-    print("{:c}".format(x),end="")
+for a in range(122, 96, -1):
+    if a % 2 is 1:
+        a -= 32
+    print("{:c}".format(a), end="")
