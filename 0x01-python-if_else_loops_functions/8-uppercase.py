4614e1b471951926ed2ae8023c246e9d070ae828
Osigwe Chidozie <chivy4gud@gmail.com>
2021-11-17 13:59:09 +0100

    0x01-python-if_else_loops_functions projects

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
         if ord(x) >= 97 and ord(x) <= 122:
             print("{}".format(chr(ord(x) - 32)), end="")
         else:
-            print("{}".format(x), end="")
+            print("{:c}".format(x), end="")
     print()
