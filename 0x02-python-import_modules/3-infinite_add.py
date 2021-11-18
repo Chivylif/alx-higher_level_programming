3b231096bbb380f12b9610a3c502066e81c6ad74
Osigwe Chidozie <chivy4gud@gmail.com>
2021-11-18 11:46:05 +0100

    0x02-python-import_modules projects

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,8 +3,6 @@ if __name__ == "__main__":
     import sys
     total_sum = 0
     len = len(sys.argv) - 1
-    index = 1
-    while index <= len:
-        total_sum += int(sys.argv[index])
-        index++
+    for x in range(len):
+        total_sum += int(sys.argv[x + 1])
     print("{:d}".format(total_sum))
