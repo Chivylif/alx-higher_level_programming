e1d686eb9f82c924a225e4f3e059ab2a38e86546
Osigwe Chidozie <chivy4gud@gmail.com>
2021-11-18 11:23:30 +0100

    0x02-python-import_modules projects

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argv = sys.argv[1:]
-    argv_count = len(argv)
+    argv = sys.argv
+    argv_count = len(argv) - 1
     index = 1
     if argv_count is 0:
         print("{:d} arguments.".format(argv_count))
