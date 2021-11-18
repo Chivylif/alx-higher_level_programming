e14e5127bade5cc1e38e533b62ae09bb08e96aaa
Osigwe Chidozie <chivy4gud@gmail.com>
2021-11-18 12:58:33 +0100

    0x02-python-import_modules projects

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -15,9 +15,13 @@ if __name__ == "__main__":
         b = int(sys.argv[3])
         if sys.argv[2] is "+":
             print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+            exit(1)
         elif sys.argv[2] is "-":
             print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+            exit(1)
         elif sys.argv[2] is "*":
             print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+            exit(1)
         elif sys.argv[2] is "/":
             print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+            exit(1)
