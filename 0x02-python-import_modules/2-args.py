ce6bec3deb708585960856bdac1d70e7a80317e7
Osigwe Chidozie <chivy4gud@gmail.com>
2021-11-18 11:15:12 +0100

    0x02-python-import_modules projects

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,15 +2,15 @@
 if __name__ == "__main__":
     import sys
     argv = sys.argv[1:]
-    argv_len = len(argv)
+    argv_count = len(argv)
     index = 1
-    if argv_len is 0:
-        print("{:d} arguments.".format(argv_len))
-    elif argv_len is 1:
-        print("{:d} argument:".format(argv_len))
+    if argv_count is 0:
+        print("{:d} arguments.".format(argv_count))
+    elif argv_count is 1:
+        print("{:d} argument:".format(argv_count))
         print("{:d}: {:s}".format(index, sys.argv[1]))
     else:
-        print("{:d} arguments:".format(argv_len))
-        while index <= argv_len:
+        print("{:d} arguments:".format(argv_count))
+        while index <= argv_count:
             print("{:d}: {:s}".format(index, sys.argv[index]))
             index += 1
