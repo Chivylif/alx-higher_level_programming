095307db7c4f4af1aae7acf80e5253300f046e0d
Osigwe Chidozie <chivy4gud@gmail.com>
2021-11-18 12:55:08 +0100

    0x02-python-import_modules projects

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if argv_count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif sys.argv[2] not in operators:
+    elif sys.argv[2] is not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
