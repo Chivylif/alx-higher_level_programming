3bc54ba4271711cd8223a6000cecc28ed7f74ee4
Osigwe Chidozie <chivy4gud@gmail.com>
2021-11-22 14:57:55 +0100

    0x03-python-data_structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,4 +3,4 @@ def no_c(my_string):
     new_string = ""
     for i in my_string:
         if i is not 'c' and i is not 'C':
-            new_string = new_string = i
+            new_string = new_string + i
