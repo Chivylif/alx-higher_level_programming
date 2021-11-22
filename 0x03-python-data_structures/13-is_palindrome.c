3ba94fb236be3deafda05471e926d084c4eeed3a
Osigwe Chidozie <chivy4gud@gmail.com>
2021-11-22 16:12:34 +0100

    0x03-python-data_structures

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,59 +1,24 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <strings.h>
 
 /**
- * reverse_listint - reverses a singly-linked list
- * 
- * @head: head list
- * 
- * Return: a pointer to the first node of the reversed list
+ * is_palindrome - checks if palindrome
+ * @head: head of node
+ * Return: 0 if not, 1 if is
  */
 
-listint_t *reverse_listint(listint_t **head)
-{
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-
-	while (current != NULL)
-	{
-		listint_t *next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-	*head = prev;
-	return (*head);
-}
-
-/**
- * is_palindrome - a function that checks if a singly linked
- * list is a palindrome
- * 
- * @head: head list
- * 
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
- */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head;
-	listint_t *reverse = NULL;
-	listint_t *temp = NULL;
-	int i = 0;
+	unsigned int len = 1;
+	listint_t *temp;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL|| *head == NULL)
 		return (1);
 
-	reverse = reverse_listint(head);
-	temp = reverse;
-
-	while (current != NULL)
+	temp = *head;
+	while (temp) /* get len of list */
 	{
-		if (current->n != temp->n)
-			return (0);
-		current = current->next;
 		temp = temp->next;
+		len++;
 	}
-	return (1);
+	return (0);
 }
